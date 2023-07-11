6c19c434e1c101f17c10a772a7a2d607c51bbfd8
Stephanie <eng.steph@gmail.com>
2023-07-10 22:20:34 -0400

    Initial inverted pendulum example

---
+++ inverted-pendulum/rt_thread.cc
@@ -0,0 +1,75 @@
+#include "rt_thread.h"
+
+double RtThread::ReadSensor(int64_t cycle_time)
+{
+  const double dt = static_cast<double>(cycle_time) / 1E9;
+
+  const double g = 9.81; // m / s^2
+
+  // Calculate anglular acceleration
+  double alpha = g / length_ * sin(current_position_);
+
+  // Multiply by dt to get the change in angular velocity, and add the velocity command
+  current_velocity_ += alpha * dt + velocity_command_;
+
+  // Multiply by dt to get change in current position
+  current_position_ += current_velocity_ * dt;
+
+  // The pendulum has hit the floor :(
+  if (abs(current_position_) > M_PI_2)
+  {
+    current_position_ = std::clamp(current_position_, -M_PI_2, M_PI_2);
+    current_velocity_ = -0.4 * current_velocity_; // Bounce!
+  }
+
+  // TODO: add sensor noise
+  return current_position_;
+}
+
+double RtThread::GetCommand(const double current_position, const double desired_position, int64_t cycle_time)
+{
+  const double dt = static_cast<double>(cycle_time) / 1E9;
+
+  // Calculate error between desired and current position
+  double error = desired_position - current_position;
+
+  // Calculate integral of error with saturation
+  error_sum_ = std::clamp(error_sum_ + error * dt, -100.0, 100.0);
+
+  // Calculate PID control law
+  double position_command = kp_ * error + ki_ * error_sum_ + kd_ * (error - prev_error_) / dt;
+
+  // Update previous error
+  prev_error_ = error;
+
+  // Divide by dt to get the output as a velocity
+  double velocity_command = std::clamp(position_command / dt, -M_PI_2, M_PI_2);
+
+  return velocity_command;
+}
+
+void RtThread::WriteCommand(const double output)
+{
+  velocity_command_ = output;
+}
+
+void RtThread::BeforeRun()
+{
+  current_position_ = initial_position_;
+}
+
+bool RtThread::Loop(int64_t ellapsed_ns) noexcept
+{
+  const double ellapsed_ms = static_cast<double>(ellapsed_ns) / 1'000'000.0;
+  const int64_t cycle_time_ns = ellapsed_ns - prev_ns_;
+  prev_ns_ = ellapsed_ns;
+
+  const double current_position = ReadSensor(cycle_time_ns);
+  const double output = GetCommand(current_position, 0, cycle_time_ns);
+  WriteCommand(output);
+
+  data_logger_->EmplaceData(ellapsed_ms, current_position);
+
+  ++iterations_;
+  return iterations_ >= max_iterations_;
+}
---
+++ inverted-pendulum/rt_thread.h
@@ -0,0 +1,51 @@
+#ifndef CACTUS_RT_EXAMPLES_MESSAGE_PASSING_EXAMPLE_RT_THREAD_H_
+#define CACTUS_RT_EXAMPLES_MESSAGE_PASSING_EXAMPLE_RT_THREAD_H_
+
+#include <cactus_rt/rt.h>
+
+#include <memory>
+
+#include "data_logger_thread.h"
+
+using cactus_rt::CyclicThread;
+using cactus_rt::schedulers::Fifo;
+
+class RtThread : public CyclicThread<>
+{
+  std::shared_ptr<DataLogger> data_logger_;
+  size_t max_iterations_;
+  size_t iterations_ = 0;
+  int64_t prev_ns_ = 0;
+
+  // Pendulum properties
+  const double initial_position_ = 0.3; // Initial position of the pendulum in rad, 0 indicates top
+  const double length_ = 0.5;           // Length of the pendulum in meters
+  double current_position_;
+  double current_velocity_ = 0; // Assume the pendulum starts from stationary
+  double velocity_command_ = 0;
+
+  // Controller properties
+  const double kp_ = 5E-4;
+  const double ki_ = 0;
+  const double kd_ = 1E-5;
+  double error_sum_ = 0;
+  double prev_error_ = 0;
+
+  double ReadSensor(int64_t ellapsed_ns);
+  double GetCommand(const double current_position, const double desired_position, int64_t ellapsed_ns);
+  void WriteCommand(const double output);
+
+public:
+  RtThread(std::shared_ptr<DataLogger> data_logger, int period_ns, size_t max_iterations = 30000)
+      : CyclicThread<>("RtThread", period_ns, Fifo::Config{80 /* priority */}),
+        data_logger_(data_logger),
+        max_iterations_(max_iterations)
+  {
+  }
+
+protected:
+  void BeforeRun() override;
+  bool Loop(int64_t ellapsed_ns) noexcept final;
+};
+
+#endif
