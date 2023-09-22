be5a41f7a2faf6e3672b913df4c771743eed9c35
Oren Bell <oren.bell@wustl.edu>
2023-09-22 10:36:51 -0500

    Added camera demo to exercise 4

---
+++ exercise4/src/camera_demo/src/actuation_node.cpp
@@ -0,0 +1,52 @@
+// Listen to the /stop topic and stop the robot when a message is received
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/empty.hpp"
+
+using namespace std::chrono_literals;
+
+
+class ActuationNode : public rclcpp::Node
+{
+public:
+  ActuationNode()
+  : Node("actuation_node")
+  {
+    // Create a subscription on the "stop" topic
+    subscription_ = this->create_subscription<std_msgs::msg::Empty>(
+      "stop", 10, std::bind(&ActuationNode::stop_robot, this, std::placeholders::_1));
+  }
+
+private:
+  void stop_robot(const std_msgs::msg::Empty::SharedPtr)
+  {
+    // Stop the robot
+    RCLCPP_INFO(this->get_logger(), "STOPPING ROBOT");
+    bool stop = true;
+  }
+
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr subscription_;
+  bool stop = false;
+};
+
+int main() {
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<ActuationNode>();
+
+  // Set the priority of this thread to be real-time
+  sched_param sch;
+  sch.sched_priority = 50;
+  if (sched_setscheduler(0, SCHED_FIFO, &sch) == -1) {
+    perror("sched_setscheduler failed");
+    exit(-1);
+  }
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
