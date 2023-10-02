e90b33cbf7a85ecf2362c69a77163ed32de9de44
Oren Bell <oren.bell@wustl.edu>
2023-10-02 17:20:51 -0500

    Refactored architecture of exercise4

--- exercise4/src/camera_demo/src/sensor_node.cpp
+++ exercise4/src/camera_demo/src/sensor_node.cpp
@@ -4,157 +4,75 @@
 #include <memory>
 #include <string>
 #include <vector>
-#include <cactus_rt/tracing.h>
+#include <list>
+#include <thread>
 
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
-#include "std_msgs/msg/empty.hpp"
+#include "camera_demo/sensor_node.hpp"
 
 using namespace std::chrono_literals;
 
-using FileSink = cactus_rt::tracing::FileSink;
-using TraceAggregator = cactus_rt::tracing::TraceAggregator;
-using ThreadTracer = cactus_rt::tracing::ThreadTracer;
 
-
-class SensorNode : public rclcpp::Node
+SensorNode::SensorNode() : Node("subscriber_node")
 {
-public:
-  SensorNode()
-  : Node("subscriber_node")
-  {
-    StartTracing("actuation_node", "actuation_node.json");
-    trace_aggregator = nullptr;
-    main_tracer = std::make_shared<ThreadTracer>("main_thread");
-    logging_tracer = std::make_shared<ThreadTracer>("logging_thread");
+  objdet_tracer_ = std::make_shared<ThreadTracer>("main_thread");
+  logging_tracer_ = std::make_shared<ThreadTracer>("logging_thread");
 
-    // Create callback groups
-    realtime_group_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
-    besteffort_group_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  // Create a subscription on the "camera" topic to log the image
+  subscription_logger_ = this->create_subscription<sensor_msgs::msg::Image>(
+    "camera", 10, std::bind(&SensorNode::data_logger, this, std::placeholders::_1));
 
-    // Create subscription options
-    rclcpp::SubscriptionOptions realtime_options;
-    realtime_options.callback_group = realtime_group_;
-    rclcpp::SubscriptionOptions besteffort_options;
-    besteffort_options.callback_group = besteffort_group_;
+  // Create a subscription on the "camera" topic to detect an object
+  subscription_object_ = this->create_subscription<sensor_msgs::msg::Image>(
+    "camera", 10, std::bind(&SensorNode::object_detector, this, std::placeholders::_1));
 
-    // Create a subscription on the "camera" topic to log the image
-    subscription_logger_ = this->create_subscription<sensor_msgs::msg::Image>(
-      "camera", 10, std::bind(&SensorNode::data_logger, this, std::placeholders::_1),
-      besteffort_options);
+  // Create a publisher on the "stop" topic
+  publisher_ = this->create_publisher<std_msgs::msg::Empty>("stop", 10);
+}
 
-    // Create a subscription on the "camera" topic to detect an object
-    subscription_object_ = this->create_subscription<sensor_msgs::msg::Image>(
-      "camera", 10, std::bind(&SensorNode::object_detector, this, std::placeholders::_1),
-      realtime_options);
+SensorNode::~SensorNode() {
+}
 
-    // Create a publisher on the "stop" topic
-    publisher_ = this->create_publisher<std_msgs::msg::Empty>("stop", 10);
+rclcpp::CallbackGroup::SharedPtr SensorNode::get_realtime_cbg() {
+  if (!realtime_group_) {
+    realtime_group_ = this->create_callback_group(
+      rclcpp::CallbackGroupType::MutuallyExclusive);
   }
+  return realtime_group_;
+}
 
-  rclcpp::CallbackGroup::SharedPtr get_realtime_cbg() {
-    if (!realtime_group_) {
-      realtime_group_ = this->create_callback_group(
-        rclcpp::CallbackGroupType::MutuallyExclusive);
-    }
-    return realtime_group_;
+rclcpp::CallbackGroup::SharedPtr SensorNode::get_besteffort_cbg() {
+  if (!besteffort_group_) {
+    besteffort_group_ = this->create_callback_group(
+      rclcpp::CallbackGroupType::MutuallyExclusive);
   }
+  return besteffort_group_;
+}
 
-  rclcpp::CallbackGroup::SharedPtr get_besteffort_cbg() {
-    if (!besteffort_group_) {
-      besteffort_group_ = this->create_callback_group(
-        rclcpp::CallbackGroupType::MutuallyExclusive);
-    }
-    return besteffort_group_;
+void SensorNode::data_logger(const sensor_msgs::msg::Image::SharedPtr msg)
+{
+  auto span = logging_tracer_->WithSpan("data_logger");
+
+  // Print the image encoding
+  RCLCPP_INFO(this->get_logger(), "Image encoding: %s", msg->encoding.c_str());
+
+  // Print the image dimensions
+  RCLCPP_INFO(this->get_logger(), "Image dimensions: %dx%d", msg->width, msg->height);
+}
+
+void SensorNode::object_detector(const sensor_msgs::msg::Image::SharedPtr msg)
+{
+  auto span = objdet_tracer_->WithSpan("object_detector");
+  
+  // Detect an object in the image
+  if (msg->data[0] == 1) {
+    // Publish a message to /stop
+    RCLCPP_INFO(this->get_logger(), "Object detected, stopping robot");
+
+    // Create a new message
+    auto msg = std::make_unique<std_msgs::msg::Empty>();
+
+    // Publish the message
+    publisher_->publish(std::move(msg));
   }
-
-private:
-  void StartTracing(const char* app_name, const char* filename) {
-    // Enable the tracing.
-    cactus_rt::tracing::EnableTracing();
-
-    // Create the trace aggregator that will pop the queues and write the events to sinks.
-    trace_aggregator = std::make_unique<TraceAggregator>(app_name);
-
-    // Create the file sink so the data aggregated by the TraceAggregator will be written to somewhere.
-    auto file_sink = std::make_shared<FileSink>(filename);
-    trace_aggregator->RegisterSink(file_sink);
-
-    trace_aggregator->Start();
-  }
-
-  void StopTracing() {
-    cactus_rt::tracing::DisableTracing();
-
-    trace_aggregator->RequestStop();
-    trace_aggregator->Join();
-    trace_aggregator = nullptr;  // Destroy the trace aggregator and free all resources.
-  }
-
-  void data_logger(const sensor_msgs::msg::Image::SharedPtr msg)
-  {
-    // Print the image encoding
-    RCLCPP_INFO(this->get_logger(), "Image encoding: %s", msg->encoding.c_str());
-
-    // Print the image dimensions
-    RCLCPP_INFO(this->get_logger(), "Image dimensions: %dx%d", msg->width, msg->height);
-  }
-
-  void object_detector(const sensor_msgs::msg::Image::SharedPtr msg)
-  {
-    // Detect an object in the image
-    if (msg->data[0] == 1) {
-      // Publish a message to /stop
-      RCLCPP_INFO(this->get_logger(), "Object detected, stopping robot");
-
-      // Create a new message
-      auto msg = std::make_unique<std_msgs::msg::Empty>();
-
-      // Publish the message
-      publisher_->publish(std::move(msg));
-    }
-  }
-
-  // Supply these for callback groups
-  std::unique_ptr<TraceAggregator> trace_aggregator;
-  std::shared_ptr<ThreadTracer> main_tracer;
-  std::shared_ptr<ThreadTracer> logging_tracer;
-  rclcpp::CallbackGroup::SharedPtr realtime_group_;
-  rclcpp::CallbackGroup::SharedPtr besteffort_group_;
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_logger_;
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_object_;
-  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr publisher_;
-};
-
-int main() {
-  rclcpp::init(0, nullptr);
-  auto node = std::make_shared<SensorNode>();
-
-  // Put each subscriber in two different callback groups
-  rclcpp::CallbackGroup::SharedPtr data_logger_group = node->get_besteffort_cbg();
-  rclcpp::CallbackGroup::SharedPtr object_detector_group = node->get_realtime_cbg();  // OMIT IN EXERCISE
-
-  // Create an executor to put the callback groups in
-  rclcpp::executors::SingleThreadedExecutor data_logging_executor;
-  rclcpp::executors::SingleThreadedExecutor object_detector_executor;  // OMIT IN EXERCISE
-  data_logging_executor.add_callback_group(data_logger_group, node->get_node_base_interface());
-  object_detector_executor.add_callback_group(object_detector_group, node->get_node_base_interface());  // OMIT IN EXERCISE
-
-  // OMIT IN EXERCISE
-  // Create thread for object detector executor
-  std::thread object_detector_thread([&object_detector_executor]() {
-    // Give this thread real-time priority
-    sched_param sch;
-    sch.sched_priority = 50;
-    if (sched_setscheduler(0, SCHED_FIFO, &sch) == -1) {
-      perror("sched_setscheduler failed");
-      exit(-1);
-    }
-    object_detector_executor.spin();
-  });
-
-  // OMIT IN EXERCISE
-  // Run the data logger in the main thread, without realtime priority
-  data_logging_executor.spin();
-  return 0;
 }
