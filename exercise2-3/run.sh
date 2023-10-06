8711fcd5c9785c5020aaa029510ed4a9128e24b7
Shuhao Wu <shuhao@shuhaowu.com>
2023-10-06 16:26:06 -0400

    Updated exercise 2 run scripts
    cc @stephanie-eng

--- exercise2-3/run.sh
+++ exercise2-3/run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+if [ "$(basename $(pwd))" != "exercise2-3" ]; then
+  echo "ERROR: please make sure you run this command in the exercise workspace" >&2
+  exit 1
+fi
+
+if [ ! -f install/setup.bash ]; then
+  echo 'ERROR: colcon build has not finished running yet. Please run `colcon build` first.' >&2
+  exit 1
+fi
+
+if [ -n "$COLCON_PREFIX_PATH" ]; then
+  echo 'ERROR: You have already sourced install/setup.bash. To avoid potential conflict, please create a new terminal and use ./run.sh instead!' >&2
+  exit 1
+fi
+
 source install/setup.bash
 
 if [ -d /opt/ros2-rt-rpi4/ ]; then
